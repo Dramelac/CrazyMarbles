9e8a684c2d6e10c09d4b157f8af73b7fa9b15b41
Dramelac <mathieu.calemard@hotmail.fr>
2016-03-06 21:55:24 +0100

    opti Quad Wall position

--- CrazyMarble/Plateau/Cell.cpp
+++ CrazyMarble/Plateau/Cell.cpp
@@ -11,26 +11,26 @@ Cell::Cell() : heightLevel(0), quad(Quads, 4), quadWallRight(Quads, 4), quadWall
     unsigned int width = TextureLoader::tile.getSize().x;
     unsigned int height = TextureLoader::tile.getSize().y;
 
-    quad[0].texCoords = sf::Vector2f(0, 0);
-    quad[1].texCoords = sf::Vector2f(width, 0);
-    quad[2].texCoords = sf::Vector2f(width, height);
-    quad[3].texCoords = sf::Vector2f(0, height);
+    quad[0].texCoords = Vector2f(0, 0);
+    quad[1].texCoords = Vector2f(width, 0);
+    quad[2].texCoords = Vector2f(width, height);
+    quad[3].texCoords = Vector2f(0, height);
     quad.resize(width * height * 4);
 
 
     width = TextureLoader::tileWall.getSize().x;
     height = TextureLoader::tileWall.getSize().y;
 
-    quadWallRight[0].texCoords = sf::Vector2f(0, 0);
-    quadWallRight[1].texCoords = sf::Vector2f(width, 0);
-    quadWallRight[2].texCoords = sf::Vector2f(width, height);
-    quadWallRight[3].texCoords = sf::Vector2f(0, height);
+    quadWallRight[0].texCoords = Vector2f(0, 0);
+    quadWallRight[1].texCoords = Vector2f(width, 0);
+    quadWallRight[2].texCoords = Vector2f(width, height);
+    quadWallRight[3].texCoords = Vector2f(0, height);
     quadWallRight.resize(width * height * 4);
 
-    quadWallLeft[0].texCoords = sf::Vector2f(0, 0);
-    quadWallLeft[1].texCoords = sf::Vector2f(width, 0);
-    quadWallLeft[2].texCoords = sf::Vector2f(width, height);
-    quadWallLeft[3].texCoords = sf::Vector2f(0, height);
+    quadWallLeft[0].texCoords = Vector2f(0, 0);
+    quadWallLeft[1].texCoords = Vector2f(width, 0);
+    quadWallLeft[2].texCoords = Vector2f(width, height);
+    quadWallLeft[3].texCoords = Vector2f(0, height);
     quadWallLeft.resize(width * height * 4);
 
 
@@ -47,20 +47,21 @@ void Cell::setupQuadPlace(int middle, int row, int column, int max) {
     int x = (middle/2 - (row * (width/2))) + column * (width/2) - width/2;
     int y =  row * (width/4) + column * (height/4) + level + 50;
 
-    quad[0].position = sf::Vector2f(x+width/2, y);
-    quad[1].position = sf::Vector2f(x+width, y+height/4);
-    quad[2].position = sf::Vector2f(x+width/2, y+height/2);
-    quad[3].position = sf::Vector2f(x, y+height/4);
+    quad[0].position = Vector2f(x+width/2, y);
+    quad[1].position = Vector2f(x+width, y+height/4);
+    quad[2].position = Vector2f(x+width/2, y+height/2);
+    quad[3].position = Vector2f(x, y+height/4);
 
-    quadWallRight[0].position = sf::Vector2f(x+width/2, y+height/2);
-    quadWallRight[1].position = sf::Vector2f(x+width, y+height/4);
-    quadWallRight[2].position = sf::Vector2f(x+width, max-height/4);
-    quadWallRight[3].position = sf::Vector2f(x+width/2, max);
 
-    quadWallLeft[0].position = sf::Vector2f(x, y+height/4);
-    quadWallLeft[1].position = sf::Vector2f(x+width/2, y+height/2);
-    quadWallLeft[2].position = sf::Vector2f(x+width/2, max);
-    quadWallLeft[3].position = sf::Vector2f(x, max-height/4);
+    quadWallRight[0].position = quad[2].position;
+    quadWallRight[1].position = quad[1].position;
+    quadWallRight[2].position = Vector2f(x+width, max-height/4);
+    quadWallRight[3].position = Vector2f(x+width/2, max);
+
+    quadWallLeft[0].position = quad[3].position;
+    quadWallLeft[1].position = quad[2].position;
+    quadWallLeft[2].position = Vector2f(x+width/2, max);
+    quadWallLeft[3].position = Vector2f(x, max-height/4);
 }
 
 void Cell::drawCel(RenderWindow *windows) {
