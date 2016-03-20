1d4ef304fb3db716b54227c9d3b653cb99c8ec81
Dramelac <203697@supinfo.com>
2016-03-20 15:40:08 +0100

    3d project success

---
+++ irrlicht/irrlicht/main.cpp
@@ -0,0 +1,30 @@
+#include <irrlicht.h>
+
+using namespace irr;
+
+int main(void) {
+
+	IrrlichtDevice *device = createDevice( // creation device
+		video::EDT_OPENGL,                      // l'API est OpenGL
+		core::dimension2d<u32>(800, 600),   // taille de la fenetre 800x600
+		32, false, true, false, 0);
+
+	video::IVideoDriver* driver = device->getVideoDriver();                    // creation driver
+	scene::ISceneManager* sceneManager = device->getSceneManager();                  // creation scene manager
+
+	sceneManager->addCameraSceneNode(0,             // ajout camera fixe
+		core::vector3df(0, 0, 0),
+		core::vector3df(5, 0, 0));
+
+	while (device->run()) {                          // la boucle de rendu
+		driver->beginScene(                          // demarre le rendu
+			true,                                    // clear back-buffer
+			true,                                    // clear z-buffer
+			video::SColor(155, 155, 155, 255));    // fond blanc
+		sceneManager->drawAll();                    // calcule le rendu
+		driver->endScene();                         // affiche le rendu
+	}
+
+	device->drop();                                 // liberation de la memoire
+	return 0;
+}
