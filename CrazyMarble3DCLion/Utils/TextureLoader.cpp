0f187094703b5cbcd830bb2e172816fa8cfe76e2
Dramelac <mathieu.calemard@hotmail.fr>
2016-03-28 16:34:16 +0200

    uniform model name

--- CrazyMarble3DCLion/Utils/TextureLoader.cpp
+++ CrazyMarble3DCLion/Utils/TextureLoader.cpp
@@ -18,7 +18,7 @@ void TextureLoader::LoadingTextures(IVideoDriver* driver, ISceneManager* sceneMa
     tile = driver->getTexture("data/model/cube_tex.png");
     sphere = driver->getTexture("data/model/sphere_c.png");
 
-    cellMesh = sceneManager->getMesh("data/model/cube.3ds");
+    cellMesh = sceneManager->getMesh("data/model/Cell_flat.3ds");
     cell_pente_Mesh = sceneManager->getMesh("data/model/Cell_pente.3ds");
     cell_angle_Mesh = sceneManager->getMesh("data/model/Cell_angle.3ds");
     sphereMesh = sceneManager->getMesh("data/model/sphere.3ds");
