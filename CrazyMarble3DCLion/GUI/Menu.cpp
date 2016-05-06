995a0e40cbf41b11c32f2e0f7ae96a7da8e39f36
Dramelac <mathieu.calemard@hotmail.fr>
2016-05-06 03:07:39 +0200

    refacto img

--- CrazyMarble3DCLion/GUI/Menu.cpp
+++ CrazyMarble3DCLion/GUI/Menu.cpp
@@ -14,28 +14,28 @@ Menu::Menu(IrrlichtDevice *inDevice, KeyboardEvent *keyEvent)
     device->getCursorControl()->setVisible(true);
 
 
-    background = gui->addImage(driver->getTexture("data/GUI/BGCM2.png"), position2d<int>(0, 0));
+    background = gui->addImage(driver->getTexture("data/GUI/Menu/BGCM2.png"), position2d<int>(0, 0));
 
     exit = gui->addButton(rect<s32>(1800,950,1900,1000), 0, 101, L"Quit", L"Exits Program");
 
     play = gui->addButton(rect<s32>(657,400,1263,545), 0, 102, L"");
-    play->setImage(driver->getTexture("data/GUI/test_bouton_avec_fond_play.png"));
-    play->setPressedImage(driver->getTexture("data/GUI/bouton_main_menu_play_pressed.png"));
+    play->setImage(driver->getTexture("data/GUI/Menu/test_bouton_avec_fond_play.png"));
+    play->setPressedImage(driver->getTexture("data/GUI/Menu/bouton_main_menu_play_pressed.png"));
     play->setDrawBorder(false);
 
     scoreBoard = gui->addButton(rect<s32>(657,550,1263,695), 0, 103, L"");
-    scoreBoard->setImage(driver->getTexture("data/GUI/test_bouton_avec_fond_scoreboard.png"));
-    scoreBoard->setPressedImage(driver->getTexture("data/GUI/bouton_main_menu_scoreboard_pressed.png"));
+    scoreBoard->setImage(driver->getTexture("data/GUI/Menu/test_bouton_avec_fond_scoreboard.png"));
+    scoreBoard->setPressedImage(driver->getTexture("data/GUI/Menu/bouton_main_menu_scoreboard_pressed.png"));
     scoreBoard->setDrawBorder(false);
 
     levelEditor = gui->addButton(rect<s32>(657, 700, 1263, 845),0,104, L"");
-    levelEditor->setImage(driver->getTexture("data/GUI/bouton_main_menu_level_editor_selected.png"));
-    levelEditor->setPressedImage(driver->getTexture("data/GUI/bouton_main_menu_level_editor_pressed.png"));
+    levelEditor->setImage(driver->getTexture("data/GUI/Menu/bouton_main_menu_level_editor_selected.png"));
+    levelEditor->setPressedImage(driver->getTexture("data/GUI/Menu/bouton_main_menu_level_editor_pressed.png"));
     levelEditor->setDrawBorder(false);
 
     credit = gui->addButton(rect<s32>(657, 850, 1263, 995),0,104, L"");
-    credit->setImage(driver->getTexture("data/GUI/bouton_main_menu_credits_unselected.png"));
-    credit->setPressedImage(driver->getTexture("data/GUI/bouton_main_menu_credits_pressed.png"));
+    credit->setImage(driver->getTexture("data/GUI/Menu/bouton_main_menu_credits_unselected.png"));
+    credit->setPressedImage(driver->getTexture("data/GUI/Menu/bouton_main_menu_credits_pressed.png"));
     credit->setDrawBorder(false);
     //play->setScaleImage(true);
     //NickMenu nickMenu(device, keyEvent);
