3f35a038b831b0a45b3c129c7884a5e1c5bc875a
Dramelac <mathieu.calemard@hotmail.fr>
2016-05-07 23:24:40 +0200

    fix life count

--- CrazyMarble3DCLion/Play/BlackMarbles.cpp
+++ CrazyMarble3DCLion/Play/BlackMarbles.cpp
@@ -6,7 +6,7 @@
 #include "../Utils/TextureLoader.h"
 
 
-BlackMarbles::BlackMarbles(ISceneManager *sceneManager, vector3df position, s32 id) : Entities("BlackMarble", 80)  {
+BlackMarbles::BlackMarbles(ISceneManager *sceneManager, vector3df position, s32 id) : Entities("BlackMarble", 60)  {
     sceneMesh = TextureLoader::sphereMesh;
     sceneNode = sceneManager->addMeshSceneNode(sceneMesh);
     sceneNode->setMaterialTexture(0, TextureLoader::sphereBlack);
@@ -18,7 +18,7 @@ BlackMarbles::BlackMarbles(ISceneManager *sceneManager, vector3df position, s32 
 
 BlackMarbles::BlackMarbles(IMeshSceneNode *node) {
     sceneNode = node;
-    Entities(node->getName(), 80);
+    Entities(node->getName(), 60);
 }
 
 
