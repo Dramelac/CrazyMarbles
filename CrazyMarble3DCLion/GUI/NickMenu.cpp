3b189d6ddbf56b62fa98d137da2a99d4750c2872
antoinesimonin <209745@supinfo.com>
2016-05-08 01:38:01 +0200

    ckean update

--- CrazyMarble3DCLion/GUI/NickMenu.cpp
+++ CrazyMarble3DCLion/GUI/NickMenu.cpp
@@ -17,9 +17,9 @@ NickMenu::NickMenu(IrrlichtDevice *device, KeyboardEvent *keyEvent) :
     device->getCursorControl()->setVisible(true);
 
 
-    gui->addImage(driver->getTexture("data/GUI/BGCM.png"), position2d<int>(0, 0));
+    gui->addImage(driver->getTexture("data/GUI/Menu/BGCM.png"), position2d<int>(0, 0));
 
-    welcome = gui->addImage(driver->getTexture("/data/GUI/Menu/BGCM2.png"), position2d<int>(0, 0));
+    welcome = gui->addImage(driver->getTexture("data/GUI/Menu/BGCM2.png"), position2d<int>(0, 0));
     exit = gui->addButton(rect<s32>(1800,950,1900,1000), 0, 101, L"Quit", L"Exits Program");
     valide = gui->addButton(rect<s32>(600,550,1320,650), 0, 103, L"Valider");
 
@@ -43,8 +43,6 @@ void NickMenu::loop() {
 
             gui->drawAll();
 
-            stringc nickname  = editBox->getText();
-
             driver->endScene();
 
             if (exit->isPressed()){
@@ -52,6 +50,7 @@ void NickMenu::loop() {
 
             } else if (valide->isPressed()){
 
+                stringc nickname  = editBox->getText();
                 std::cout << nickname.c_str()  << std::endl ;
 
                 return;
