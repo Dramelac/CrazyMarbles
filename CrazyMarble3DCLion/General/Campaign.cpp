c95bbca7f671bfcf66d4e8a40382615d07a82b20
Dramelac <mathieu.calemard@hotmail.fr>
2016-05-09 23:31:26 +0200

    add class

---
+++ CrazyMarble3DCLion/General/Campaign.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mathieu on 09/05/16.
+//
+
+#include "Campaign.h"
---
+++ CrazyMarble3DCLion/General/Campaign.h
@@ -0,0 +1,14 @@
+//
+// Created by mathieu on 09/05/16.
+//
+
+#ifndef CRAZYMARBLE3DCLION_CAMPAIGN_H
+#define CRAZYMARBLE3DCLION_CAMPAIGN_H
+
+
+class Campaign {
+
+};
+
+
+#endif //CRAZYMARBLE3DCLION_CAMPAIGN_H
