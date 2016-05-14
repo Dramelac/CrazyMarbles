57e486f9be2821d07feea29f69ba25ec7c4cadd9
Dramelac <mathieu.calemard@hotmail.fr>
2016-05-14 15:46:41 +0200

    first implement campaign loop

--- CrazyMarble3DCLion/General/Campaign.cpp
+++ CrazyMarble3DCLion/General/Campaign.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "Campaign.h"
+#include "Game.h"
+
+Campaign::Campaign(IrrlichtDevice* device, KeyboardEvent* keyEvent) {
+    this->device = device;
+    driver = device->getVideoDriver();
+    this->keyEvent = keyEvent;
+}
+
+
+void Campaign::play() {
+
+    for (int mapNumber = 0; mapNumber < mapCycle.size(); ++mapNumber) {
+        bool restart = true;
+        while (restart){
+            Game game(device, keyEvent, mapCycle[mapNumber]);
+            switch (game.gameLoop()) {
+                // next level
+                case 0:
+                    restart = false;
+                    break;
+                // exit game
+                case -1:
+                    return;
+                // restart game
+                default:
+                    break;
+            }
+        }
+    }
+}
+
--- CrazyMarble3DCLion/General/Campaign.h
+++ CrazyMarble3DCLion/General/Campaign.h
@@ -5,9 +5,27 @@
 #ifndef CRAZYMARBLE3DCLION_CAMPAIGN_H
 #define CRAZYMARBLE3DCLION_CAMPAIGN_H
 
+#include <irrlicht.h>
+#include "../Utils/KeyboardEvent.h"
+
+using namespace irr;
+using namespace io;
+using namespace core;
+using namespace video;
 
 class Campaign {
 
+private:
+    IrrlichtDevice* device;
+    IVideoDriver* driver;
+    KeyboardEvent* keyEvent;
+
+    array<path> mapCycle;
+
+public:
+    Campaign(IrrlichtDevice* device, KeyboardEvent* keyEvent);
+
+    void play();
 };
 
 
