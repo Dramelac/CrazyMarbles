f138b673c647d5588a63087fbb580fa7832be004
Dramelac <mathieu.calemard@hotmail.fr>
2016-05-15 01:27:57 +0200

    update type

--- CrazyMarble3DCLion/Utils/FileSystemUtils.h
+++ CrazyMarble3DCLion/Utils/FileSystemUtils.h
@@ -5,9 +5,16 @@
 #ifndef CRAZYMARBLE3DCLION_FILESYSTEMUTILS_H
 #define CRAZYMARBLE3DCLION_FILESYSTEMUTILS_H
 
+#include <dirent.h>
+#include <irrlicht.h>
+
+using namespace irr;
+using namespace core;
 
 class FileSystemUtils {
-
+public:
+    static bool CheckExistingDir(stringc path);
+    static array<stringc> parseDirectory(stringc path);
 };
 
 
