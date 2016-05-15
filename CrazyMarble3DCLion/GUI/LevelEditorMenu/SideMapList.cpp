51a9dc177c7ba57505cfe44988b34f8c69b06192
Dramelac <mathieu.calemard@hotmail.fr>
2016-05-15 15:23:12 +0200

    add + button

--- CrazyMarble3DCLion/GUI/LevelEditorMenu/SideMapList.cpp
+++ CrazyMarble3DCLion/GUI/LevelEditorMenu/SideMapList.cpp
@@ -8,8 +8,18 @@ SideMapList::SideMapList(IGUIEnvironment* gui, IVideoDriver* driver) {
     title = gui->addImage(driver->getTexture("data/GUI/LevelEditor/Menu/Campaign.png"), position2d<int>(250, 0));
     array<path> pathList = campaign.getMapCycle();
 
-    for (u16 i = 0; i < pathList.size(); ++i) {
+    u16 i =0;
+    for (i = 0; i < pathList.size(); ++i) {
         listButton.push_back(new LvlEditorMapButton(gui, driver, pathList[i], i));
+    }
+
+    addMap = gui->addButton(rect<s32>(vector2d<s32>(i*100+500,15), dimension2d<s32>(100,50)));
+    addMap->setImage(driver->getTexture("data/GUI/LevelEditor/Menu/plus.png"));
+    addMap->setUseAlphaChannel(true);
+    addMap->setDrawBorder(false);
+
+    if (i >= 10){
+        addMap->setVisible(false);
     }
 
     saving = gui->addButton(rect<s32>(vector2d<s32>(1500,15), dimension2d<s32>(100,50)),0,-1,L"Saving campaign");
@@ -19,6 +29,7 @@ SideMapList::SideMapList(IGUIEnvironment* gui, IVideoDriver* driver) {
 SideMapList::~SideMapList() {
     title->remove();
     saving->remove();
+    addMap->remove();
     for (int i = 0; i < listButton.size(); ++i) {
         delete listButton[i];
     }
