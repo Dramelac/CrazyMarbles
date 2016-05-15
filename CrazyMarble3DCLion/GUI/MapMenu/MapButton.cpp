b3fb60101d3250efb378b9013e46d720d532ab99
Dramelac <mathieu.calemard@hotmail.fr>
2016-05-15 02:08:43 +0200

    change button name with fileName

--- CrazyMarble3DCLion/GUI/MapMenu/MapButton.cpp
+++ CrazyMarble3DCLion/GUI/MapMenu/MapButton.cpp
@@ -5,9 +5,11 @@
 #include "MapButton.h"
 
 MapButton::MapButton(const path &map, u16 nPos, IGUIEnvironment* gui) : map(map) {
-    vector2d<s32> pos((nPos%5)*350+150, (nPos/5)*200+250);
+    vector2d<s32> pos((nPos%5)*300+200, (nPos/5)*150+250);
     dimension2d<s32> size(250,100);
-    myButton = gui->addButton(rect<s32>(pos, size),0,-1,L"cc");
+    stringw fileName = map.subString((u32)map.findLastChar("/") + 1, map.size());
+    fileName = fileName.subString(0, fileName.size()-4);
+    myButton = gui->addButton(rect<s32>(pos, size), 0, -1, fileName.c_str());
 }
 
 bool MapButton::checkPressed() {
