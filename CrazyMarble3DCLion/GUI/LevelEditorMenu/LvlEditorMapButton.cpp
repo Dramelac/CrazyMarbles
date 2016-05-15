9a4275fa1fd89688c0514738ad1deab77a2b687f
Dramelac <mathieu.calemard@hotmail.fr>
2016-05-15 14:18:10 +0200

    change button size

--- CrazyMarble3DCLion/GUI/LevelEditorMenu/LvlEditorMapButton.cpp
+++ CrazyMarble3DCLion/GUI/LevelEditorMenu/LvlEditorMapButton.cpp
@@ -6,10 +6,10 @@
 
 LvlEditorMapButton::LvlEditorMapButton(IGUIEnvironment* gui, IVideoDriver* driver, const path &map, u16 nPos): pos(nPos) {
     mapButton = new MapButton(map, gui,
-                              vector2d<s32>(nPos*100+500, 0),
-                              dimension2d<s32>(100,100));
+                              vector2d<s32>(nPos*100+500, 15),
+                              dimension2d<s32>(100,50));
 
-    removeButton = gui->addButton(rect<s32>(vector2d<s32>(nPos*100+575, 0),
+    removeButton = gui->addButton(rect<s32>(vector2d<s32>(nPos*100+575, 15),
                                             dimension2d<s32>(25,25)));
     removeButton->setImage(driver->getTexture("data/GUI/LevelEditor/Menu/remove.png"));
 
