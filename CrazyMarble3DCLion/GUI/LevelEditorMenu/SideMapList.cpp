e22738af5767025c46cb37e96a6110a0abea9604
Dramelac <mathieu.calemard@hotmail.fr>
2016-05-15 03:25:03 +0200

    add new level editor gui class

---
+++ CrazyMarble3DCLion/GUI/LevelEditorMenu/SideMapList.cpp
@@ -0,0 +1,9 @@
+//
+// Created by mathieu on 15/05/16.
+//
+
+#include "SideMapList.h"
+
+SideMapList::SideMapList(IGUIEnvironment* gui) {
+    
+}
---
+++ CrazyMarble3DCLion/GUI/LevelEditorMenu/SideMapList.h
@@ -0,0 +1,22 @@
+//
+// Created by mathieu on 15/05/16.
+//
+
+#ifndef CRAZYMARBLE3DCLION_SIDEMAPLIST_H
+#define CRAZYMARBLE3DCLION_SIDEMAPLIST_H
+
+#include <irrlicht.h>
+
+using namespace irr;
+using namespace gui;
+using namespace core;
+
+class SideMapList {
+
+
+public:
+    SideMapList(IGUIEnvironment* gui);
+};
+
+
+#endif //CRAZYMARBLE3DCLION_SIDEMAPLIST_H
