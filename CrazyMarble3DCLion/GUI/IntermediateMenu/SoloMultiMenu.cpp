0efa82f407fab76cf139978c3b3cd8a7ca6e5ee5
Dramelac <mathieu.calemard@hotmail.fr>
2016-05-16 22:10:49 +0200

    add solomulti class

---
+++ CrazyMarble3DCLion/GUI/IntermediateMenu/SoloMultiMenu.cpp
@@ -0,0 +1,12 @@
+//
+// Created by mathieu on 16/05/16.
+//
+
+#include "SoloMultiMenu.h"
+
+SoloMultiMenu::SoloMultiMenu(IrrlichtDevice *device, KeyboardEvent *keyEvent)
+        : GUIBase(device, keyEvent) {
+
+    
+
+}
---
+++ CrazyMarble3DCLion/GUI/IntermediateMenu/SoloMultiMenu.h
@@ -0,0 +1,18 @@
+//
+// Created by mathieu on 16/05/16.
+//
+
+#ifndef CRAZYMARBLE3DCLION_SOLOMULTIMENU_H
+#define CRAZYMARBLE3DCLION_SOLOMULTIMENU_H
+
+
+#include "../GUIBase.h"
+
+class SoloMultiMenu : public GUIBase {
+
+public:
+    SoloMultiMenu(IrrlichtDevice *device, KeyboardEvent *keyEvent);
+};
+
+
+#endif //CRAZYMARBLE3DCLION_SOLOMULTIMENU_H
