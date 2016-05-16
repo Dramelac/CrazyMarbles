555d8a52ff79080f26005ec06ffd4b61b9601f61
Dramelac <mathieu.calemard@hotmail.fr>
2016-05-16 18:53:40 +0200

    set font on message

--- CrazyMarble3DCLion/GUI/PlayMessage/WinLooseChoose.cpp
+++ CrazyMarble3DCLion/GUI/PlayMessage/WinLooseChoose.cpp
@@ -13,6 +13,7 @@ WinLooseChoose::WinLooseChoose(IrrlichtDevice *device, KeyboardEvent *keyEvent, 
 
     this->message = gui->addStaticText(message.c_str(),rect<s32>(vector2d<s32>(200,80), dimension2d<s32>(200,400)), false, true, background);
     this->message->setOverrideColor(SColor(255,255,255,255));
+    this->message->setOverrideFont(gui->getFont("data/GUI/fonts/font15px.png"));
 
     restart = gui->addButton(rect<s32>(vector2d<s32>(30,200), dimension2d<s32>(125,50)), background, -1, L"Restart");
     quit = gui->addButton(rect<s32>(vector2d<s32>(175,200), dimension2d<s32>(125,50)), background, -1, L"Exit");
@@ -44,7 +45,7 @@ s16 WinLooseChoose::loop() {
 
         driver->endScene();
 
-        if (quit->isPressed() ||keyEvent->IsKeyDown(KEY_ESCAPE, true)){
+        if (quit->isPressed()){
             return -1;
         }
         if (win && next->isPressed()){
