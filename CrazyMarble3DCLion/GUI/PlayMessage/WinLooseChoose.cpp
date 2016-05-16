bfcf90e67dfce0e66e491bee272ad8eedd585180
Dramelac <mathieu.calemard@hotmail.fr>
2016-05-16 14:38:24 +0200

    add new class popup end stage

---
+++ CrazyMarble3DCLion/GUI/PlayMessage/WinLooseChoose.cpp
@@ -0,0 +1,7 @@
+//
+// Created by mathieu on 16/05/16.
+//
+
+#include "WinLooseChoose.h"
+
+WinLooseChoose::WinLooseChoose(IrrlichtDevice *device, KeyboardEvent *keyEvent) : GUIBase(device, keyEvent) { }
---
+++ CrazyMarble3DCLion/GUI/PlayMessage/WinLooseChoose.h
@@ -0,0 +1,20 @@
+//
+// Created by mathieu on 16/05/16.
+//
+
+#ifndef CRAZYMARBLE3DCLION_WINLOOSECHOOSE_H
+#define CRAZYMARBLE3DCLION_WINLOOSECHOOSE_H
+
+
+#include "../GUIBase.h"
+
+class WinLooseChoose : public GUIBase {
+private:
+    
+
+public:
+    WinLooseChoose(IrrlichtDevice *device, KeyboardEvent *keyEvent);
+};
+
+
+#endif //CRAZYMARBLE3DCLION_WINLOOSECHOOSE_H
