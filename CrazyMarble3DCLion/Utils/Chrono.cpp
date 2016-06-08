d92c09240d5b33d7eeffcf7fc8583b18963f81f3
antoinesimonin <209745@supinfo.com>
2016-06-08 14:48:58 +0200

    add back tiùer

--- CrazyMarble3DCLion/Utils/Chrono.cpp
+++ CrazyMarble3DCLion/Utils/Chrono.cpp
@@ -11,7 +11,7 @@ Chrono::Chrono(IrrlichtDevice* inDevice, u32 chrono, IVideoDriver* driver) {
     timer->setTime(0);
     timer->setSpeed(1);
     displayChrono = inDevice->getGUIEnvironment()->addStaticText(L"60",rect<s32>(885,20,935,80));
-    backTimer->setImage(driver->getTexture("data/GUI/Menu/play/timer.png"));
+    backTimer = inDevice->getGUIEnvironment()->addImage(driver->getTexture("data/GUI/Menu/play/timer.png"), vector2d<s32>(880,15));
 }
 
 Chrono::~Chrono() {
@@ -19,6 +19,7 @@ Chrono::~Chrono() {
     timer->setTime(0);
     timer->setSpeed(1);
     displayChrono->remove();
+    backTimer->remove();
 }
 
 u32 Chrono::getTime() {
