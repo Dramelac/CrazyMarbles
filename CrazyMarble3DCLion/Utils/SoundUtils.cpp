318b93647634dbe57e596da4fefea8c5976daeb7
Dramelac <mathieu.calemard@hotmail.fr>
2016-06-10 02:52:46 +0200

    fix compile + noise option

--- CrazyMarble3DCLion/Utils/SoundUtils.cpp
+++ CrazyMarble3DCLion/Utils/SoundUtils.cpp
@@ -13,20 +13,15 @@ void SoundUtils::play() {
         std::cout << "fail engine loading" << std::endl;
         return;
     }
-    std::cout << "sound" << std::endl;
     engine->play2D("data/Sound/makeourstory.wav");
-    engine->
 
-    return;
 }
 
 void SoundUtils::muteSoundBack() {
     if(statusSound){
         engine->setAllSoundsPaused(false);
         statusSound = false;
-
     }else{
-
         engine->setAllSoundsPaused(true);
         statusSound = true;
     }
@@ -38,26 +33,34 @@ void SoundUtils::getDamage() {
         engine->play2D("data/Sound/pop.wav");
     }
 }
+
 void SoundUtils::muteNoise() {
     noise = !noise;
 }
 
 void SoundUtils::winSound() {
-    engine->play2D("data/Sound/win.wav");
+    if(noise) {
+        engine->play2D("data/Sound/win.wav");
+    }
 }
 
 
 void SoundUtils::looseSound() {
-    engine->play2D("data/Sound/loose.wav");
-
+    if(noise) {
+        engine->play2D("data/Sound/loose.wav");
+    }
 }
 
 void SoundUtils::timerSound() {
-    engine->play2D("data/Sound/timer.wav");
+    if(noise) {
+        engine->play2D("data/Sound/timer.wav");
+    }
 }
 
 void SoundUtils::fallSound() {
-    engine->play2D("data/Sound/fall.wav");
+    if(noise) {
+        engine->play2D("data/Sound/fall.wav");
+    }
 }
 
 
